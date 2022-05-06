fba13266e90e45e2c627b20a7115eca81ba16b64
Robin Khiv <43227798+RobinKhiv@users.noreply.github.com>
2022-05-06 10:47:36 -0700

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-sqlite3 ./var/user.db < ./share/usershard.sql
 sqlite3 ./var/game1.db < ./share/game1shard.sql
 sqlite3 ./var/game2.db < ./share/game2shard.sql
 sqlite3 ./var/game3.db < ./share/game3shard.sql
